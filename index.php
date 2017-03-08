cb9b3614af8ab8d8a1b190fa8bbf8e913d9719bd
Jesse Fender <jf0772256@otc.edu>
2017-03-08 14:30:31 -0600

    More changes
    Decided to use index.php as a pure controller and include views as
    nessisary.

--- index.php
+++ index.php
@@ -5,14 +5,5 @@
   //The developers name is Jesse Fender.
   //Started 03/08/2017.
 ?>
-<?php
-include("resources/partial/_head_preTitle.php");
-?>
-<title>This is the home page</title>
-<?php include("resources/partial/_head_PostTitle.php"); ?>
 
-<div class="jumbotron">
-  <h1>Hello World!</h1>
-</div>
-
-<?php include("resources/partial/_end_Page.php") ?>
+<?php include("resources/view/home.php"); ?>
---
+++ resources/view/home.php
@@ -0,0 +1,12 @@
+
+<?php
+include("resources/partial/_head_preTitle.php");
+?>
+<title>This is the home page</title>
+<?php include("resources/partial/_head_PostTitle.php"); ?>
+
+<div class="jumbotron">
+  <h1>Hello World!</h1>
+</div>
+
+<?php include("resources/partial/_end_Page.php") ?>
