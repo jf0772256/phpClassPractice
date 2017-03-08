620b396b150ed5db551fa4d942c9d60abd9cb5f9
Jesse Fender <jf0772256@otc.edu>
2017-03-08 14:27:43 -0600

    Correction
    I p[laced code in the wrong index.php - corrected

--- resources/index.php
+++ resources/index.php
@@ -1,15 +1,3 @@
 <?php
 //this is a protected folder and will need to force people out of here if they find their way in. with out the controllers help
 ?>
-
-<?php
-include("resources/partial/_head_preTitle.php");
-?>
-<title>This is the home page</title>
-<?php include("resources/partial/_head_PostTitle.php"); ?>
-
-<div class="jumbotron">
-  <h1>Hello World!</h1>
-</div>
-
-<?php include("resources/partial/_end_Page.php") ?>
