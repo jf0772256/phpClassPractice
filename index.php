564dcb66e82bf6b52d986c888462a7618423690e
Jesse Fender <jf0772256@otc.edu>
2017-03-08 13:44:50 -0600

    Initial Commit starting folers and a new index.php file
    Starting out the folders and the files with in initial commit and will
    be used and added to through out.

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  //This is the guest controller
+  //users will directed and use the authenticated users controller.
+  //the puropose of this project is to practice more MVC and now Implement use of classes to work with the site
+  //The developers name is Jesse Fender.
+  //Started 03/08/2017.
+?>
