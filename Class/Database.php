c92f1e0e53aca45c3adfb6c57d0f3cba233ae22b
Jesse Fender <jf0772256@otc.edu>
2017-03-27 08:26:43 -0500

    resuming work on PHP class
    Updated some code in the Database Class -> most notibly added code to
    check if a table exists before modifications are completed on it.
    Updated the rename table value to include new check, this eliminates the
    need to error check as the code will only run if the table exists.

--- Class/Database.php
+++ Class/Database.php
@@ -56,6 +56,92 @@ class DatabaseClass
   public function setDBPrefix($prefixValue){
     $this->dbtableprefix = $prefixValue;
   }
+
+  //now for other workhorses
+  public function checkConnection(){
+    return mysqli_ping($this->dbC);
+  }
+
+  public function create_newTable($tableName, $tableParams){
+    //Table name expects a valid string, Table params requires an array, minus commas of strings for each column in the table.
+    if (!is_array($tableParams)){
+      throw new ErrorException("Expected array of strings.");
+      exit();
+    }else{
+      $query="CREATE TABLE IF NOT EXISTS $tableName (";
+      foreach ($tableParams as $params) {
+        $query = $query . $params . ", ";
+      }
+      //now to crop the last comma off
+      $lenofquery = strlen($query);
+      echo var_dump($lenofquery);
+      $query = substr($query, 0, ($lenofquery - 2));
+      $query = $query . ")";
+      echo var_dump(strlen($query));
+      echo var_dump($query);
+    }
+  }
+
+  public function dropTableByName($tableName){
+    $tableName = $this->dbtableprefix . $tableName;
+    $query = "DROP TABLE IF EXISTS $tableName";
+    $stmnt = $this->dbC->prepare($query);
+    $result = $stmnt->execute();
+    if (!$result){
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  public function renameTableName($oldTableName, $newTableName){
+    //assumes that you didnt include the prefix on the newTableName, You will need to have it for oldTableName
+    // uses a simple request to change teh oldTableName to newTableName and prepend the prefix, This can be used individually
+    // we will work on a renaming call for if you change the prefix.
+    $table_exists = check_preexisting_tables($oldTableName);
+    if ($table_exists) {
+      $newTableName = $this->dbtableprefix . $newTableName;
+      $query = "RENAME TABLE $oldTableName TO $newTableName";
+      $stmnt = $this->dbC->prepare($query);
+      $result = $stmnt->execute();
+      if ($result) {
+        return true;
+      }else{
+        return false;
+      }
+    }else{
+      return false;
+    }
+  }
+
+  protected function check_preexisting_tables($tableNameVar){
+    //query the database and if the table is there then retun true.
+    //if the tables are not created then return false.
+    $database = $this->dbname;
+    $query = "SELECT COUNT(*) AS Count FROM information_schema.TABLES WHERE TABLE_Schema = '$database' AND TABLE_NAME=?";
+    $stmnt = $db -> prepare($query);
+    $stmnt -> bind_param("s",$tableNameVar);
+    if (!$stmnt -> execute()){
+      $error_message = "Something went wrong and the process has stopped, contact your support contact for further assistance.";
+      include("error/db_error.php");
+      exit();
+    }
+    $tblresult = $stmnt -> get_result();
+    $tblCount = $tblresult -> fetch_assoc(); //non associated array , easier to get to result for this purpose.
+    //if statements to return either true or false...
+    if ($tblCount["Count"] === 1){
+      //table exists and the database is dirty...
+      return true;
+    }elseif ($tblCount["Count"] === 0){
+      //table does not exist and the database if clean...
+      return false;
+    }else{
+      //something has gone wrong... horribly wrong...
+      $error_message = "An unknown error has occured and we have stopped the script from running further, contact us through your POC.";
+      include("error/db_error.php");
+      exit();
+    }
+  }
 }
 
 ?>
--- model/database.php
+++ model/database.php
@@ -10,4 +10,14 @@
   //now I want to see that the connection has been made.
   $db = new DatabaseClass($host, $database, $dbuser, $dbuserpw);
 
+  $db->setDBPrefix("test_");
+  echo $db->getDBPrefix();
+  echo "<br />";
+  echo $db->checkConnection();
+  $tablecol = array();
+  $tablecol[0] = "testID INT UNSIGNED NOT NULL AUTO_INCREMENT";
+  $tablecol[1] = "testUserName VARCHAR(30) NOT NULL";
+  $tablecol[2] = "testFlag TINYINT(1) DEFAULT 0";
+  $tablecol[3] = "PRIMARY KEY (testID)";
+  $db->create_newTable("test", $tablecol);
 ?>
