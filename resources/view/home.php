08a31e482bbfe5c92751a3d7897f23393895bd85
Jesse Fender <jf0772256@otc.edu>
2017-04-05 02:29:25 -0500

    fixed typo
    corrected a typo

--- resources/view/home.php
+++ resources/view/home.php
@@ -8,7 +8,7 @@ include("resources/partial/_head_preTitle.php");
 
 <div class="jumbotron">
   <h1>This is the home page</h1>
-  <p>This is to dimonstrate the usefullness of includes and views.</p>
+  <p>This is to demonstrate the usefullness of includes and views.</p>
 </div>
 
 <?php include("resources/partial/_end_Page.php") ?>
