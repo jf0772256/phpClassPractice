54ab1725f20b1492a19113f20f0288d7dbef8785
Jesse Fender <jf0772256@otc.edu>
2017-04-07 18:12:55 -0500

    implemented Fluid Interfaces to query builder and testing now
    I have been working on cleaning up the queryBuilder class, and testing
    it in the testClass.php/qbtest.php now can start chaining query builder
    functions :)

--- Class/testClass.php
+++ Class/testClass.php
@@ -1,6 +1,6 @@
 <?php
 // test class for use of new query builder class idea.
-include "Database.php";
+// include ("Database.php");
 
 class testQueryBuilder extends DatabaseClass
 {
@@ -12,6 +12,9 @@ class testQueryBuilder extends DatabaseClass
   private $dbPassword;
   private $tablePrefix;
   private $dbport;
+  private $queryString;
+  private $mode;
+
 
   function __construct($ndbhost = "localhost", $ndbname = "", $ndbusername = "", $ndbuserpassword = "", $ndbtableprefix = " ", $ndbport = 3600){
     //default Constructor
@@ -24,6 +27,71 @@ class testQueryBuilder extends DatabaseClass
 
     parent::__construct($this->host, $this->dbName, $this->dbUser, $this->dbPassword, $this->tablePrefix, $this->dbport);
     $this->dbC = parent::getdbconnection();
+  }
+
+  public function queryStart($mode){
+    // is the start of the query
+    if ($mode == "SELECT") {
+      $this->queryString = "SELECT ";
+    }elseif ($mode == "UPDATE") {
+      $this->queryString = "UPDATE ";
+    }elseif ($mode == "DELETE") {
+      $this->queryString = "DELETE ";
+    }elseif ($mode == "INSERT") {
+      $this->queryString = "INSERT ";
+    }elseif ($mode == "CREATE") {
+      $this->queryString = "CREATE ";
+    }elseif ($mode == "DROP") {
+      $this->queryString = "DROP ";
+    }elseif ($mode == "ALTER") {
+      $this->queryString = "ALTER ";
+    }elseif ($mode == "RENAME") {
+      $this->queryString = "RENAME ";
+    }
+    return $this;
+  }
+
+  public function ddlStatement($ddl2ndpart){
+    //if a ddl statement then accept then second part of the ddl statement
+    if ($this->queryString == "CREATE " || $this->queryString == "DROP " || $this->queryString == "ALTER " || $this->queryString == "RENAME "){
+      $this->queryString .= $this->dbC->real_escape_string(htmlspecialchars($ddl2ndpart)) . " ";
+    }
+    return $this;
+  }
+
+  public function set_table_name($tablesArray="", $tableVar =""){
+    //tablesArray needs array -> or will fail, it's optional
+    //tableVar needs single value, it to is optional BUT
+    //One or the other MUST be used.
+    if (empty($tablesArray) && empty($tableVar)) {
+      throw new Exception("Error Processing Request: Expecting an array or a string, none were sent with your request;");
+    }elseif(!empty($tablesArray) && empty($tableVar)) {
+      if (!is_array($tablesArray)) {
+        throw new Exception("Error Processing Request: Expecting an Array containing only strings. If you are wishing to send one table sent an empty array param '[]' followed by your desired table name;");
+      }else{
+        //do array stuff
+        foreach ($tablesArray as $tName) {
+          $tName = $this->dbC->real_escape_string(htmlspecialchars($tName));
+          $this->queryString .= $tName . ", ";
+        }
+        $this->queryString = parent::cropStringValue($this->queryString,2);
+      }
+    }elseif(empty($tablesArray) && !empty($tableVar)) {
+      if (!is_string($tableVar)) {
+        throw new Exception("Error Processing Request: Expecting a string value, but something else was passed. Please ensure that you are using a string. If you encounter further trouble please review the documentation;");
+      }else{
+        //do string stuff
+        $tableVar = $this->dbC->real_escape_string(htmlspecialchars($tableVar));
+        $this->queryString .= $tableVar;
+      }
+    }elseif (!empty($tablesArray) && !empty($tableVar)) {
+      throw new Exception("Error Processing Request: Only one value can be accepted, either in array form or in string form, for further information see documentation;");
+    }
+    return $this;
+  }
+
+  public function get_query_string(){
+    return $this->queryString;
   }
 }
 ?>
--- model/qbtest.php
+++ model/qbtest.php
@@ -1,4 +1,16 @@
 <?php
   require("./Class/Database.php");
   require("./Class/testClass.php");
+
+  $host = 'localhost';
+  $database = 'phpclasses';
+  $dbuser = 'phpClasses';
+  $dbuserpw = 'test1234';
+
+  $qb = new testQueryBuilder($host, $database, $dbuser, $dbuserpw);
+  $qb->setDBPrefix("test_");
+
+  $query = $qb->queryStart("CREATE")->ddlStatement("TABLE")->set_table_name([],"valueTable")->get_query_string();
+  //$query = $qb->get_query_string();
+  echo $query;
 ?>
