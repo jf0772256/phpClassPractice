3f55b647f199f2ebdef63c888004b48bdfba29be
Jesse Fender <jf0772256@otc.edu>
2017-04-11 13:33:17 -0500

    Extended interfaces into single interface

---
+++ Class/Interface/DataType.Interface.php
@@ -0,0 +1,6 @@
+<?php
+// I am hoping to get this to implement the other datatype interfaces.
+interface DataType extends DateTime,String,Text,Numeric,PrimaryKey,ForeignKey,Index {
+  //I think that this will work...
+}
+?>
